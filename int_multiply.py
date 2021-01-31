9048a956587846f020d5171a45f1939c2f74f7a6
sivaram <sivaram031.md@gmail.com>
2021-01-31 20:30:07 +0530

    exit to my pycharm in ubuntu

--- int_multiply.py
+++ int_multiply.py
@@ -3,3 +3,6 @@ x = int(input('enter number : '))
 for i in range(x):
     if i[x]%2 ==0:
      print(x)
+
+
+
--- sample.py
+++ sample.py
@@ -1,1 +1,9 @@
-print('hello')
+
+
+s1 = 'how are you'
+s2 = 'what are you doing'
+l1 = s1.split()
+l2 = s2.split()
+
+print(set(l1) & set(l2))
+
